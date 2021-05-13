32c2194e0acf5083835bf3438ef35ec3cbbc41a3
maxcet <maxcetdex@gmail.com>
2021-05-13 21:53:08 +0400

    fix calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -7,6 +7,6 @@ void	*ft_calloc(size_t num, size_t size)
 	new = (void*)malloc(num*size);
 	if (new == NULL)
 		return(NULL);
-	ft_bzero(new, 0, num*size);
+	ft_bzero(new, num*size);
 	return(new);
 }

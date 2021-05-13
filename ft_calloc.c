172f00c26f982527b529d87aa78d7ad48090f005
maxcet <maxcetdex@gmail.com>
2021-05-13 20:33:31 +0400

    Change Calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -4,9 +4,9 @@ void	*ft_calloc(size_t num, size_t size)
 {
 	void *new;
 
-	new = malloc(num*size);
+	new = (void*)malloc(num*size);
 	if (new == NULL)
 		return(NULL);
-	ft_memset(new, 0, num*size);
+	ft_bzero(new, 0, num*size);
 	return(new);
 }

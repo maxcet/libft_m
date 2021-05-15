1179866bbe995bae2a87af61af76312a63d02755
maxcet <maxcetdex@gmail.com>
2021-05-15 07:51:46 +0400

    rewrite isalpa, isdigit, tolower, toupper.

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,16 +1,8 @@
 #include "libft.h"
 
-static int	ft_isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
-
 int	ft_tolower(int c)
 {
-	if (ft_isupper(c))
+	if (c >= 'A' && c <= 'Z')
 		c = c - 'A' + 'a';
 	return (c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -1,16 +1,8 @@
 #include "libft.h"
 
-static int	ft_islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-}
-
 int	ft_toupper(int c)
 {
-	if (ft_islower(c))
+	if ((c >= 'a' && c <= 'z'))
 		c = c - 'a' + 'A';
 	return (c);
 }
